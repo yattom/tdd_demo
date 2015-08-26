61894d340c8db9965f816e2ffccc5044abcc67e2
yattom <tsutomu.yasui@gmail.com>
2015-08-26 11:17:31 +0900

    add get_last

--- todoapp/todo.py
+++ todoapp/todo.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+from datetime import datetime
+
 TODO = 0
 
 todos = []
@@ -7,6 +9,17 @@ todos = []
 def get_all():
     return todos
 
-def put(text, state, due):
-    item = {'text': text, 'state': state, 'due': due}
+def put(text, state, due, add_at=None):
+    if not add_at:
+        add_at = datetime.now()
+    item = {'text': text, 'state': state, 'add_at': add_at, 'due': due}
     todos.append(item)
+
+def get_last():
+    if not todos:
+        return None
+    return todos[-1]
+
+def clear():
+    global todos
+    todos = []
--- todoapp/todo_test.py
+++ todoapp/todo_test.py
@@ -1,7 +1,10 @@
 # coding: utf-8
 
 from todoapp import todo
-from datetime import datetime
+from datetime import datetime, timedelta
+
+def setup_function(fn):
+    todo.clear()
 
 def test_最初は空():
     assert todo.get_all() == []
@@ -9,9 +12,32 @@ def test_最初は空():
 def test_TODOの属性():
     todo.put(text='TODO1',
              state=todo.TODO,
+             add_at=datetime(2015, 8, 26, 10, 0, 0),
              due=datetime(2015, 8, 26, 12, 0, 0))
     assert todo.get_all() == [{'text': 'TODO1',
                                'state': todo.TODO,
+                               'add_at': datetime(2015, 8, 26, 10, 0, 0),
                                'due': datetime(2015, 8, 26, 12, 0, 0)},
                              ]
 
+def test_TODOの属性_add_atは現在時刻():
+    todo.put(text='TODO1',
+             state=todo.TODO,
+             due=datetime(2015, 8, 26, 12, 0, 0))
+    actual = todo.get_last()['add_at']
+    now = datetime.now()
+    assert now - actual <= timedelta(seconds=1)
+
+def test_最後の項目を取得する_空の場合():
+    assert todo.get_last() == None
+
+def test_最後の項目を取得する():
+    todo.put(text='TODO1',
+             state=todo.TODO,
+             add_at=datetime(2015, 8, 26, 10, 0, 0),
+             due=datetime(2015, 8, 26, 12, 0, 0))
+    assert todo.get_last() == {'text': 'TODO1',
+                               'state': todo.TODO,
+                               'add_at': datetime(2015, 8, 26, 10, 0, 0),
+                               'due': datetime(2015, 8, 26, 12, 0, 0)}
+                              

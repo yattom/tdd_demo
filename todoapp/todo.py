1e25922883d82078516b629cf75481c006b886b3
yattom <tsutomu.yasui@gmail.com>
2015-08-26 10:57:09 +0900

    define todo attributes

--- todoapp/todo.py
+++ todoapp/todo.py
@@ -1,9 +1,12 @@
 # coding: utf-8
 
+TODO = 0
+
 todos = []
 
 def get_all():
     return todos
 
-def put(todo):
-    todos.append(todo)
+def put(text, state, due):
+    item = {'text': text, 'state': state, 'due': due}
+    todos.append(item)
--- todoapp/todo_test.py
+++ todoapp/todo_test.py
@@ -1,10 +1,17 @@
 # coding: utf-8
 
 from todoapp import todo
+from datetime import datetime
 
 def test_最初は空():
     assert todo.get_all() == []
 
-def test_追加したものを取得できる():
-    todo.put('TODO1')
-    assert todo.get_all() == ['TODO1']
+def test_TODOの属性():
+    todo.put(text='TODO1',
+             state=todo.TODO,
+             due=datetime(2015, 8, 26, 12, 0, 0))
+    assert todo.get_all() == [{'text': 'TODO1',
+                               'state': todo.TODO,
+                               'due': datetime(2015, 8, 26, 12, 0, 0)},
+                             ]
+

0176a05cc2fbe6167fc4dce26604e2df51ead504
yattom <tsutomu.yasui@gmail.com>
2015-08-26 11:27:55 +0900

    refactoring test

--- todoapp/todo_test.py
+++ todoapp/todo_test.py
@@ -3,41 +3,57 @@
 from todoapp import todo
 from datetime import datetime, timedelta
 
+
+class Helper:
+    @staticmethod
+    def 初期化():
+        todo.clear()
+
+    @staticmethod
+    def TODO追加(text, state=None, add_at=None, due=None):
+        if not state:
+            state = todo.TODO
+        todo.put(text=text, state=state, add_at=add_at, due=due)
+
+    @staticmethod
+    def すべてのTODO():
+        return todo.get_all()
+
+    @staticmethod
+    def 最後のTODO():
+        return todo.get_last()
+
 def setup_function(fn):
-    todo.clear()
+    Helper.初期化()
 
 def test_最初は空():
     assert todo.get_all() == []
 
 def test_TODOの属性():
-    todo.put(text='TODO1',
-             state=todo.TODO,
-             add_at=datetime(2015, 8, 26, 10, 0, 0),
-             due=datetime(2015, 8, 26, 12, 0, 0))
-    assert todo.get_all() == [{'text': 'TODO1',
-                               'state': todo.TODO,
-                               'add_at': datetime(2015, 8, 26, 10, 0, 0),
-                               'due': datetime(2015, 8, 26, 12, 0, 0)},
-                             ]
+    Helper.TODO追加(text='TODO1',
+                    state=todo.TODO,
+                    add_at=datetime(2015, 8, 26, 10, 0, 0),
+                    due=datetime(2015, 8, 26, 12, 0, 0))
+    assert Helper.すべてのTODO() == [{'text': 'TODO1',
+                                      'state': todo.TODO,
+                                      'add_at': datetime(2015, 8, 26, 10, 0, 0),
+                                      'due': datetime(2015, 8, 26, 12, 0, 0)},
+                                    ]
 
 def test_TODOの属性_add_atは現在時刻():
-    todo.put(text='TODO1',
-             state=todo.TODO,
-             due=datetime(2015, 8, 26, 12, 0, 0))
-    actual = todo.get_last()['add_at']
+    Helper.TODO追加(text='TODO1')
+    actual = Helper.最後のTODO()['add_at']
     now = datetime.now()
     assert now - actual <= timedelta(seconds=1)
 
 def test_最後の項目を取得する_空の場合():
-    assert todo.get_last() == None
+    assert Helper.最後のTODO() == None
 
 def test_最後の項目を取得する():
-    todo.put(text='TODO1',
-             state=todo.TODO,
-             add_at=datetime(2015, 8, 26, 10, 0, 0),
-             due=datetime(2015, 8, 26, 12, 0, 0))
-    assert todo.get_last() == {'text': 'TODO1',
-                               'state': todo.TODO,
-                               'add_at': datetime(2015, 8, 26, 10, 0, 0),
-                               'due': datetime(2015, 8, 26, 12, 0, 0)}
-                              
+    Helper.TODO追加(text='TODO1')
+    assert Helper.最後のTODO()['text'] == 'TODO1'
+
+def test_最後の項目を取得する_2件の場合():
+    Helper.TODO追加(text='TODO1')
+    Helper.TODO追加(text='TODO2')
+    assert Helper.最後のTODO()['text'] == 'TODO2'

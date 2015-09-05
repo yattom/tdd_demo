d8d3d75cc7a0b0ef3575815ce4cefd3c3602203a
yattom <tsutomu.yasui@gmail.com>
2015-09-05 17:29:53 +0900

    仮実装

--- todoapp/todo.py
+++ todoapp/todo.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import sys
+import os
 
 todos = []
 
@@ -16,3 +18,14 @@ def get_last():
     if not todos:
         return None
     return todos[-1]
+
+def main():
+    if sys.argv[1] == 'add':
+        pass
+    if sys.argv[1] == 'get_last':
+        sys.stdout.write('TODO 1' + os.linesep)
+        sys.stdout.write('yattom' + os.linesep)
+        sys.stdout.write('2015/9/12' + os.linesep)
+
+if __name__=='__main__':
+    main()

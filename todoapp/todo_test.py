7ab56fc5ee37c3ea981709ce79e39b3178abb4f1
yattom <tsutomu.yasui@gmail.com>
2015-09-05 17:18:35 +0900

    コマンドライン版に向けたテスト

--- todoapp/todo_test.py
+++ todoapp/todo_test.py
@@ -2,8 +2,24 @@
 
 import pytest
 import datetime
+import subprocess
 
 from todoapp import todo
+
+def sh(cmd, with_stderr=False):
+    '''
+    テスト用のコマンド実行ヘルパー
+    '''
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    stdoutdata, stderrdata = p.communicate()
+    retcode = p.wait()
+    if retcode != 0:
+        assert False, 'sh("%s") retcode: %d'%(cmd, retcode)
+    if with_stderr:
+        return stdoutdata.decode(), stderrdata.decode()
+    else:
+        return stdoutdata.decode()
+
 
 def test_最初は空():
     assert todo.get_last() == None
@@ -27,3 +43,11 @@ def test_最後に追加した詳細を取得できる_2個追加した場合():
     todo.append('TODO 2')
     actual = todo.get_last()
     assert actual.text == 'TODO 2'
+
+def test_cmd_TODOのすべての属性を取得できる():
+    sh("python -m todoapp.todo add 'TODO 1' yattom 2015/9/12")
+    output = sh("python -m todoapp.todo get_last")
+    output_lines = [l.strip() for l in output.split('\n')]
+    assert output_lines[0] == 'TODO 1'
+    assert output_lines[1] == 'yattom'
+    assert output_lines[2] == '2015/9/12'

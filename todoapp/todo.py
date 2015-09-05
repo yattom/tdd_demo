88f2d77da2133003567a84b295980f0a9223cf71
yattom <tsutomu.yasui@gmail.com>
2015-09-05 17:00:32 +0900

    最後の詳細と追加のテストと実装

--- todoapp/todo.py
+++ todoapp/todo.py
@@ -1,4 +1,16 @@
 # coding: utf-8
 
+
+todos = []
+
+class ToDo:
+    def __init__(self, text):
+        self.text = text
+
+def append(text):
+    todos.append(ToDo('TODO 1'))
+
 def get_last():
-    return None
+    if not todos:
+        return None
+    return todos[-1]
--- todoapp/todo_test.py
+++ todoapp/todo_test.py
@@ -7,3 +7,7 @@ from todoapp import todo
 def test_最初は空():
     assert todo.get_last() == None
 
+def test_最後に追加した詳細を取得できる_1個追加した場合():
+    todo.append('TODO 1')
+    actual = todo.get_last()
+    assert actual.text == 'TODO 1'
